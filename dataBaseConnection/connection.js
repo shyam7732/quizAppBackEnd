429ac794ce50a9569aa6b656963907875bb4022d
shyam7732 <sbspersonal2000@gmail.com>
2023-08-02 14:28:36 +0530

    connection with local

--- dataBaseConnection/connection.js
+++ dataBaseConnection/connection.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
-const connection = mongoose.connect('mongodb+srv://shyam7732:s1u2m3a4n5@quizdata.bci0u3q.mongodb.net/?retryWrites=true&w=majority')
+// const connection = mongoose.connect('mongodb+srv://shyam7732:s1u2m3a4n5@quizdata.bci0u3q.mongodb.net/?retryWrites=true&w=majority')
+
+const connection = mongoose.connect('mongodb+srv://shyam7732:s1u2m3a4n5@quizdata.bci0u3q.mongodb.net/?authMechanism=SCRAM-SHA-1')
+
 
 export default connection
